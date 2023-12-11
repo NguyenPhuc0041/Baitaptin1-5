6f67256ba8c8d6a323a6091a8300a1b0eebb8301
Nguyen Phuc Duong <Nguyen Phuc Duong@DESKTOP-8T9G6GN>
2023-12-12 02:25:36 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hoctinbuoi4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Bai tap 1
+            //Console.Write("Nhap vao so nguyen n = ");
+            //int n =int.Parse(Console.ReadLine());
+            //if (n % 3 == 0) 
+            //{
+            //    Console.Write("So {0} chia het cho 3", n);
+            //}
+            //else
+            //{
+            //    Console.Write("So {0} khong chia het cho 3", n);
+            //}
+            //Console.ReadKey();
+
+            //Bai tap 2
+            //Console.WriteLine("Nhap vao so nguyen n = ");
+            //int n =int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= n; i++) 
+
+            //{
+            //    Console.WriteLine("Bang cua chuong {0}", i);
+            //    for (int j = 1; j <= 10; j++) 
+            //    {
+            //        int s = i * j;
+            //        Console.WriteLine("{0} x {1} = {2}", i, j, s);
+
+            //    }
+            //    Console.ReadLine();
+            //}
+
+            //Bai tap 3
+            //Console.WriteLine("Nhap vao so nguyen n = ");
+            //int n =int.Parse(Console.ReadLine());
+            //int s = 0;
+            //for (int i = 1; i <= n; i++)
+            //    s = s + i;
+            //Console.WriteLine("Tong tu 1 den {0} la = {1}", n, s);
+            //Console.ReadKey();
+
+            //Bai tap 4
+            //Console.WriteLine("Nhap vao so nguyen n = ");
+            //int n =int.Parse(Console.ReadLine());
+            //if (Math.Sqrt(n) % 1 ==0)
+            //{
+            //    Console.Write("So {0} la so chinh phuong", n);
+            //}
+            //else
+            //{
+            //    Console.Write("So {0} khong phai la so chinh phuong", n);
+            //}
+            //Console.ReadKey();
+
+            //Bai tap 5
+            //Console.WriteLine("Nhap vao thang bat ky : ");
+            //int thang =int.Parse(Console.ReadLine());;
+            //switch (thang)
+            //{
+            //    case 1:
+            //    case 3:
+            //    case 5:
+            //    case 7:
+            //    case 9:
+            //    case 11:
+            //        Console.Write("Thang {0} co 31 ngay", thang);
+            //        break;
+            //    case 2:
+            //        Console.Write("Thang 2 co 29 ngay vao nam nhuan va 28 ngay vao nam khong nhuan");
+            //        break;
+            //    case 4:
+            //    case 6:
+            //    case 8:
+            //    case 10:
+            //    case 12:
+            //        Console.Write("Thang {0} co 30 ngay", thang);
+            //        break;
+            //}
+            //Console.ReadKey();
+
+
+        }
+    }
+}
